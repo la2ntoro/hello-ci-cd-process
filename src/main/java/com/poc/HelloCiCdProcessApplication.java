ea34c959d96231e09ab2f93d6a8d433bcf55d7ce
lalan <la2ntoro@gmail.com>
2023-08-07 10:55:22 +0700

    Initialize Application

---
+++ src/main/java/com/poc/HelloCiCdProcessApplication.java
@@ -0,0 +1,13 @@
+package com.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCiCdProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloCiCdProcessApplication.class, args);
+	}
+
+}
